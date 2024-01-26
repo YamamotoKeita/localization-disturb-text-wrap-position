e1f3c128ba449b86536c51b0584f5af63768ca5f
Keita Yamamoto <yamamoto@altonotes.co.jp>
2024-01-26 10:20:44 +0900

    Update

--- TextWrapTest/TextWrapTestApp.swift
+++ TextWrapTest/TextWrapTestApp.swift
@@ -1,11 +1,28 @@
 import SwiftUI
+import UIKit
 
 @main
 struct TextWrapTestApp: App {
+    @State var fontNames: [String] = []
+
     var body: some Scene {
         WindowGroup {
-            Text("報酬が届かない等ございましたらお問い合わせください。")
-                .font(Font.custom("HiraginoSans-W3", size: 17))
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(fontNames, id: \.self) { fontName in
+                        VStack {
+                            Text(fontName)
+                            Text("報酬が届かない等ございましたらお問い合わせください。")
+                                .font(Font.custom(fontName, size: 17))
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                fontNames = UIFont.familyNames.flatMap { family in
+                    UIFont.fontNames(forFamilyName: family)
+                }
+            }
         }
     }
 }
