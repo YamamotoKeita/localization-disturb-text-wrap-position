02a511427578e3c9de048b0511586160f32a0dc0
Keita Yamamoto <yamamoto@altonotes.co.jp>
2024-01-31 09:26:20 +0900

    Show all fonts

--- TextWrapTest/TextWrapTestApp.swift
+++ TextWrapTest/TextWrapTestApp.swift
@@ -10,9 +10,15 @@ struct TextWrapTestApp: App {
             ScrollView {
                 VStack(spacing: 20) {
                     ForEach(fontNames, id: \.self) { fontName in
-                        VStack {
+                        VStack(alignment: .leading) {
                             Text(fontName)
+                                .padding(.vertical, 6)
+                                .frame(maxWidth: .infinity)
+                                .background(Color.black.opacity(0.2))
+
                             Text("報酬が届かない等ございましたらお問い合わせください。")
+                                .font(Font.custom(fontName, size: 17))
+                            Text(verbatim: "報酬が届かない等ございましたらお問い合わせください。")
                                 .font(Font.custom(fontName, size: 17))
                         }
                     }
